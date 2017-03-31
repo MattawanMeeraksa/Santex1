9aaed2b62c1efd109e77d7f7a4d243754d45551e
france <vorachit.1998@mail.kmutt.ac.th>
2017-03-31 22:27:15 +0700

    edit and delete list

--- JavaApplication8/src/project/DetailList.java
+++ JavaApplication8/src/project/DetailList.java
@@ -4,15 +4,200 @@
  * and open the template in the editor.
  */
 package project;
-import java.*;
+
+import com.sun.glass.ui.Cursor;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import static javax.swing.UIManager.getString;
+import static javax.xml.ws.Endpoint.create;
 
 /**
  *
  * @author user
  */
-public class DetailList extends javax.swing.JFrame {
+public class DetailList extends JFrame {
+    JButton back;
+    JLabel detail;
+    JButton delete;
+    JLabel del;
+
+    ArrayList<JButton> buttons1; //เก็บปุ่ม buttons1 เป็น arraylist 
+    ArrayList<JButton> buttons2; //เก็บปุ่ม buttons2 เป็น arraylist 
+    ArrayList<String> listPlanName,description,reps,set; //เก็บ ตัวแปร เป็น arraylist 
+    
+    public DetailList() throws ClassNotFoundException, SQLException{
+        ListPlan lp1 = new ListPlan();
+        back = new JButton("<");
+        detail = new JLabel("Detail List");
+        del = new JLabel("Delete");
+       
+        JPanel j1 = new JPanel(new GridLayout(1 ,1));
+        setTitle("Detail List");
+        JPanel j2 = new JPanel(new GridLayout(0, 2));
+        
+        ListPlan lp = new ListPlan();
+        ResultSet rs = lp.show();
+        
+        int i = 0;
+        int j = 0;
+        
+        buttons1 = new ArrayList<JButton>();
+        buttons2 = new ArrayList<JButton>();
+        delete = new JButton("Delete");
+        
+        listPlanName = new ArrayList<String>();
+        description = new ArrayList<String>();
+        reps = new ArrayList<String>();
+        set = new ArrayList<String>();
+        
+        JButton[] editbtn = new JButton[100];
+        editbtn[i].setSize(20,20);
+        JButton[] deletebtn = new JButton[100];
+        
+        JPanel top = new JPanel();
+        top.add(back);
+
+        JPanel top2 = new JPanel();
+        top.add(detail);
+        
+        JPanel alltop = new JPanel(new GridLayout(1, 1));
+        alltop.add(top, BorderLayout.WEST);
+        alltop.add(top2, BorderLayout.EAST);
+
+        add(alltop, BorderLayout.NORTH);
+
+        while (rs.next()) { //เพื่อเช็คว่า กดปุ่มของ List plan ใด
+            listPlanName.add(rs.getString("listName"));
+            description.add(rs.getString("descriptionList"));
+            reps.add(rs.getString("reps"));
+            set.add(rs.getString("set"));
+            j2.add(new JLabel("List Plan : "));
+            j2.add(new JLabel(rs.getString("listName")));
+            j2.add(new JLabel("Description : "));
+            j2.add(new JLabel(rs.getString("descriptionList")));
+            j2.add(new JLabel("Reps : "));
+            j2.add(new JLabel(rs.getString("reps")));
+            j2.add(new JLabel("Set : "));
+            j2.add(new JLabel(rs.getString("set")));
+           
+            
+        }
+            JPanel j3 = new JPanel(new GridLayout(0,2));
+            rs = lp.show();
+            while (rs.next()) {
+                editbtn[i] = new JButton("Edit");
+                editbtn[i].addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        for (int i = 0; i < buttons1.size(); i++) {
+                            if (e.getSource() == buttons1.get(i)) { // i ช่วยบอกว่ากำลังแก้ของแพลนไหนอยู่
+                                EditListPlan ep = new EditListPlan(listPlanName.get(i),description.get(i),reps.get(i),set.get(i));
+                                ep.setVisible(true);
+                                ep.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                ep.setLocationRelativeTo(null);
+                                ep.setResizable(false);
+                                setVisible(false);
+                            }
+                        }
+                    }
+                });
+                
+                j3.add(editbtn[i]);
+                buttons1.add(editbtn[i]);
+                i++;
+
+                deletebtn[j] = new JButton("Delete");
+                deletebtn[j].addActionListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        for (int j = 0; j < buttons2.size(); j++) { // j บอกว่ากำลัง delete แพลนไหนอยู่
+                            if (e.getSource() == buttons2.get(j)) {
+                                try {
+                                    Object[] options = {"Yes", "No"}; // pop-up เด้งขึ้นมาถาม มี yes,no ให้เลือก
+                                    int n = JOptionPane.showOptionDialog(delete, "Delete plan?", "Delete",
+                                            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                                            options, options[0]); // //กด yes n จะมีค่าเป็น 0 ถ้ากดปุ่มอื่นจะเป็น 1
+                                    if (n == 0) {
+                                        try {
+                                            System.out.println("Deleted");
+                                            ListPlan lp = new ListPlan();
+                                            lp.delete(listPlanName.get(j));
+                                        } catch (ClassNotFoundException ex) {
+                                            Logger.getLogger(DetailList.class.getName()).log(Level.SEVERE, null, ex);
+                                        } catch (SQLException ex) {
+                                            Logger.getLogger(DetailList.class.getName()).log(Level.SEVERE, null, ex);
+                                        }
+                                    }
+                                    DetailList dl = new DetailList();
+                                    dl.pack();
+                                    dl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                                    dl.setVisible(true);
+                                    dl.setLocationRelativeTo(null);
+                                    setVisible(false);
+                                    dl.setResizable(false);
+                                } catch (ClassNotFoundException ex) {
+                                    Logger.getLogger(DetailList.class.getName()).log(Level.SEVERE, null, ex);
+                                } catch (SQLException ex) {
+                                    Logger.getLogger(DetailList.class.getName()).log(Level.SEVERE, null, ex);
+                                }
+                            }else
+                                System.out.println("Cancel");
+                        }
+                    } 
+                });
+                j3.add(deletebtn[j]);
+                buttons2.add(deletebtn[j]);
+                j++;
+            }
+        
+        j1.add(j2, BorderLayout.WEST);
+        j1.add(j3, BorderLayout.CENTER);
+        //     add(Bank1,BorderLayout.WEST);
+        add(j1, BorderLayout.CENTER);
+        
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                AddList ad = new AddList();
+                ad.setVisible(true);
+                ad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                setVisible(false);
+                ad.setLocationRelativeTo(null);  
+            }
+        }
+        );
+    }
+        
+    
+        
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        DetailList dl = new DetailList();
+        dl.pack();
+        dl.setSize(400, 400);
+        dl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        dl.setVisible(true);
+        dl.setLocationRelativeTo(null);
+        dl.setResizable(false);
+    }
     
 }
--- JavaApplication8/src/project/EditListPlan.java
+++ JavaApplication8/src/project/EditListPlan.java
@@ -5,6 +5,7 @@
  */
 package project;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,17 +15,68 @@ import javax.swing.JFrame;
  *
  * @author user
  */
-public class AddList extends javax.swing.JFrame {
+public class EditListPlan extends javax.swing.JFrame {
+    private String newPlanName;
+    private String newDescription;
+    private int newReps;
+    private int newSet;
 
-    /**
-     * Creates new form AddList
-     */
-    public AddList() {
-        initComponents();
-        setTitle("Add List");
-        setResizable(false);
+    EditListPlan(String listPlanName,String description,String reps,String set) {
+        this();
+        newPlanName = listPlanName;
+        newDescription = description;
+        int r = Integer.parseInt(reps);
+        newReps = r;
+        int s = Integer.parseInt(set);
+        newSet = s;
+        jTextField1.setText(this.newPlanName);
+        jTextField2.setText(this.newDescription);
+        jTextField3.setText(this.newReps+"");
+        jTextField4.setText(this.newSet+"");
     }
     
+    EditListPlan() {
+        initComponents();
+        setTitle("Edit");
+        setResizable(false);
+    }
+    public String getNewPlanName() {
+        return newPlanName;
+    }
+
+    public String getNewDescription() {
+        return newDescription;
+    }
+
+    public int getNewReps() {
+        return newReps;
+    }
+
+    public int getNewSet() {
+        return newSet;
+    }
+
+    public void setNewPlanName(String newPlanName) {
+        this.newPlanName = newPlanName;
+    }
+
+    public void setNewDescription(String newDescription) {
+        this.newDescription = newDescription;
+    }
+
+    public void setNewReps(int newReps) {
+        this.newReps = newReps;
+    }
+
+    public void setNewSet(int newSet) {
+        this.newSet = newSet;
+    }
+    
+  
+    
+
+    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -34,25 +86,45 @@ public class AddList extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel5 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jTextField1 = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jTextField3 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jTextField4 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel5.setText("Reps");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" }));
+
+        jTextField1.setText("jTextField1");
+        jTextField1.setMinimumSize(new java.awt.Dimension(59, 20));
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
         jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel6.setText("Set");
+
+        jTextField2.setText("jTextField2");
+        jTextField2.setMinimumSize(new java.awt.Dimension(59, 20));
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("Save");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -61,10 +133,19 @@ public class AddList extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabel1.setText("Add List");
+        jTextField3.setText("jTextField3");
+        jTextField3.setMinimumSize(new java.awt.Dimension(59, 20));
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText(">");
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Edit");
+
+        jButton2.setText("<");
+        jButton2.setPreferredSize(new java.awt.Dimension(41, 32));
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
@@ -72,6 +153,12 @@ public class AddList extends javax.swing.JFrame {
         });
 
         jTextField4.setText("jTextField4");
+        jTextField4.setMinimumSize(new java.awt.Dimension(59, 20));
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel2.setText("Day");
@@ -82,29 +169,6 @@ public class AddList extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel4.setText("Description");
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel5.setText("Reps");
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" }));
-
-        jTextField1.setText("jTextField1");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-
-        jTextField2.setText("jTextField2");
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
-            }
-        });
-
-        jTextField3.setText("jTextField3");
-
-        jTextField5.setText("jTextField5");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -112,12 +176,12 @@ public class AddList extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jTextField2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                            .addComponent(jLabel1)
-                            .addGap(18, 18, 18)
-                            .addComponent(jButton2))
+                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(jLabel1))
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(jLabel2)
                             .addGap(26, 26, 26)
@@ -127,18 +191,15 @@ public class AddList extends javax.swing.JFrame {
                             .addGap(18, 18, 18)
                             .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addComponent(jLabel4)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jLabel6)
-                                    .addGap(28, 28, 28)
-                                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addComponent(jLabel5)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGap(44, 44, 44)
-                            .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addGap(28, 28, 28)
+                                .addComponent(jTextField4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addGap(18, 18, 18)
+                                .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
@@ -148,9 +209,9 @@ public class AddList extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
@@ -165,58 +226,54 @@ public class AddList extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel5)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        try {
-            DetailList dl = new DetailList();
-            dl.setVisible(true);
-            dl.setSize(400, 400);
-            System.out.println("List Plan created");
-            setVisible(false);
-            dl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+         setResizable(false);
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        // TODO add your handling code here:
+         setResizable(false);
     }//GEN-LAST:event_jTextField2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         try {                                         
             ListPlan lp = new ListPlan();
-            String listPlanName = jTextField1.getText();
-            String description = jTextField2.getText();
+            ResultSet rs = lp.show();
+            lp.editListPlanName(newPlanName, jTextField1.getText());
+            lp.editDescription(newDescription, jTextField2.getText());
             String r = jTextField3.getText();
             int reps = Integer.parseInt(r);
+            lp.editReps(newReps,reps);
             String s = jTextField4.getText();
             int set = Integer.parseInt(s);
-            String l  = jTextField5.getText();
-            int list = Integer.parseInt(l);
-            try {
-                lp.create(listPlanName, description, reps, set, list);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SQLException ex) {
-                Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            lp.editSet(newSet,set);
             
+            DetailList dl = new DetailList() ;
+            dl.setVisible(true);
+            dl.setSize(400, 400);
+            setVisible(false);
+            dl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EditListPlan.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(EditListPlan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
             DetailList dl = new DetailList() ;
             dl.setVisible(true);
             dl.setSize(400, 400);
@@ -224,24 +281,29 @@ public class AddList extends javax.swing.JFrame {
             setVisible(false);
             dl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EditListPlan.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            Logger.getLogger(AddList.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(EditListPlan.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        setResizable(false);
+    }//GEN-LAST:event_jTextField3ActionPerformed
+     
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField4ActionPerformed
+        setResizable(false);
+    }//GEN-LAST:event_jTextField4ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {  
-
+    public static void main(String args[]) {
+        
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AddList().setVisible(true);
+                new EditListPlan().setVisible(true);
             }
         });
     }
@@ -260,6 +322,5 @@ public class AddList extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
     // End of variables declaration//GEN-END:variables
 }
